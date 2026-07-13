ce2d536e699f7cd15b2e8796cb614fd321761dbe
Ansley Smith <ansleysmith3@outlook.com>
2026-07-13 11:22:48 -0400

    updated assignment path 1

--- hello_orders.py
+++ hello_orders.py
@@ -1,3 +1,4 @@
+from rich import print
 import pandas as pd
 
 orders = pd.read_csv("data/snack_orders.csv")
@@ -8,3 +9,5 @@ print(orders)
 print("Top snacks by quantity")
 top_snacks = orders.groupby("snack")["quantity"].sum().sort_values(ascending=False).head(3)
 print(top_snacks)
+
+print("I am running this with uv")
